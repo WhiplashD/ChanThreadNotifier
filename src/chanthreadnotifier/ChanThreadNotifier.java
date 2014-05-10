ba1ec68dff02fe8e1fe58ff7d306405c4508c65c
WhiplashD <toughtanx@hotmail.com>
2014-05-10 04:31:53 -0700

    Initial Commit

---
+++ src/chanthreadnotifier/ChanThreadNotifier.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chanthreadnotifier;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Whiplash
+ */
+public class ChanThreadNotifier {
+
+    static boolean selectstate = true;
+    static String keyword, board;
+    static int time, newliner;
+    static MonitorBoard monitor;
+    static Scanner in;
+    static String[] boards = new String[]{"a", "b", "c", "d", "e", "f", "g", "gif",
+        "h", "hr", "k", "m", "o", "p", "r", "s", "t", "u", "v", "vg", "vr", "w", "wg",
+        "i", "ic", "r9k", "s4s", "cm", "hm", "lgbt", "y", "3", "adv", "an", "asp", "biz",
+        "cgl", "ck", "co", "diy", "fa", "fit", "gd", "hc", "int", "jp", "lit", "mlp",
+        "mu", "n", "out", "po", "soc", "sp", "tg", "toy", "trv", "tv", "vp", "wsg", "x"};
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Initialize();
+        System.out.println("Press q at any time to quit, r to reset.");
+        monitor.start();
+        while (monitor.isAlive()) {
+            switch (in.next()) {
+                case "q":
+                    System.out.println("Exiting.");
+                    System.exit(1);
+                    break;
+                case "r":
+                    monitor.end();
+                    ClearConsole();
+                    Initialize();
+                    monitor.start();
+                    break;
+            }
+        }
+    }
+
+    public static void Initialize() {
+        in = new Scanner(System.in);
+        while (selectstate) {
+            System.out.println("Enter a board to monitor on (example g for /g/): ");
+            board = in.next();
+            if (Arrays.asList(boards).contains(board)) {
+                break;
+            }
+            System.out.println("Valid boards: ");
+            for (String s : boards) {
+                System.out.print(s + " ");
+                newliner++;
+                if (newliner > 5) {
+                    System.out.println();
+                    newliner = 0;
+                }
+            }
+            System.out.println();
+        }
+        System.out.print("Enter a keyword to monitor for: ");
+        keyword = in.next();
+        while (selectstate) {
+            System.out.println("Enter delay interval (in minutes, e.g. 1 for 1 minutes) for board update fetching (cannot be lower than 1 minutes: ");
+            {
+                try {
+                    time = in.nextInt();
+                    if (time >= 1) {
+                        break;
+                    }
+                } catch (Exception nfe) {
+                    in.nextLine();
+                }
+            }
+        }
+        System.out.println("Will monitor for " + keyword + " on board /" + board + "/" + " every " + time + " minutes.");
+        monitor = new MonitorBoard(keyword, board, time);
+    }
+
+    public static void ClearConsole() {
+        try {
+            String os = System.getProperty("os.name");
+
+            if (os.contains("Windows")) {
+                Runtime.getRuntime().exec("cls");
+            } else {
+                Runtime.getRuntime().exec("clear");
+            }
+        } catch (IOException ioe) {
+
+        }
+    }
+}
---
+++ src/chanthreadnotifier/MonitorBoard.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chanthreadnotifier;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+/**
+ *
+ * @author Whiplash
+ */
+public class MonitorBoard extends Thread {
+
+    String keyword, board, url;
+    long oldTime, currentTime, newTime, delay;
+    boolean running, shouldFetch;
+    Gson gson = new Gson();
+
+    public MonitorBoard(String keyword, String board, int time) {
+        this.keyword = keyword;
+        this.board = board;
+        delay = time;
+    }
+
+    @Override
+    public void run() {
+        running = true;
+        shouldFetch = true;
+        delay *= 60000;
+        while (running) {
+            while (shouldFetch) {
+
+                System.out.println("Fetching.");
+
+                try {
+
+                    System.out.println("Creating URL...");
+
+                    url = "http://a.4cdn.org/" + board + "/catalog.json";
+
+                    URL chanurl = new URL(url);
+                    System.out.println("URL created. " + url);
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(chanurl.openStream()));
+                    StringBuilder buffer = new StringBuilder();
+                    int read;
+
+                    char[] chars = new char[1024];
+
+                    System.out.println("Building string buffer...");
+
+                    while ((read = reader.read(chars)) != -1) {
+                        buffer.append(chars, 0, read);
+                    }
+                    String chanjson = buffer.toString();
+
+                    System.out.println("String buffer created. " + buffer.length());
+                    System.out.println("Creating gson object...");
+
+                    Page[] page = gson.fromJson(chanjson, Page[].class);
+
+                    System.out.println("Gson object created.");
+                    System.out.println("Searching board threads for keyword matches...");
+
+                    for (Page p : page) {
+                        for (Post post : p.threads) {
+                            try {
+                                if (post.com.toLowerCase().contains(keyword) | post.sub.toLowerCase().contains(keyword)) {
+
+                                    System.out.println("Match found at " + "boards.4chan.org/" + board + "/thread/" + post.no);
+
+                                }
+                            } catch (NullPointerException npe) {
+
+                            }
+                        }
+                    }
+
+                    System.out.println("Search finished.");
+
+                } catch (IOException | JsonSyntaxException ex) {
+
+                }
+                shouldFetch = false;
+                oldTime = System.currentTimeMillis();
+                newTime = oldTime + delay;
+
+                System.out.println("Sleeping for " + (delay /= 60000) + " minutes.");
+
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                this.running = false;
+            }
+            currentTime = System.currentTimeMillis();
+            if (currentTime >= newTime) {
+
+                System.out.println("Waking.");
+
+                shouldFetch = true;
+            }
+        }
+    }
+
+    public void end() {
+        this.running = false;
+    }
+
+    public static class Post {
+
+        int no;
+        String com;
+        String sub;
+    }
+
+    public static class Page {
+
+        int page;
+        Post[] threads;
+    }
+
+}
